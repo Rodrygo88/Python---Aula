e0e65f742b13bbd3e27bdf1d866ca8694df6c8ba
Rodrygo88 <2rodrigosilva2@gmail.com>
2024-04-02 15:02:22 -0300

    add

--- Projeto/Python/Exercício - 074.py
+++ Projeto/Python/Exercício - 074.py
@@ -1,1 +1,14 @@
-print("====== DESAFIO 074 ======")
+print("====== DESAFIO 074 ======")  
+
+import random
+
+tupla = (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10), random.randint(1, 10))
+
+print("Valores sorteados: ", end="")
+for num in tupla:
+    print(num, end=" ")    
+
+print("\n")
+
+print(f"O MAIOR número da tupla é {max(tupla)}")
+print(f"O MENOR número da tupla é {min(tupla)}")

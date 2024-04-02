c8c842b0f04dc799ec40857c698136ece9fbcb36
Rodrygo88 <2rodrigosilva2@gmail.com>
2024-04-02 12:40:25 -0300

    Delete teste.py

--- Projeto/Python/teste.py
+++
@@ -1,9 +0,0 @@
-
-nome = str(input("Nome: "))
-idade = int(input("Idade: "))
-
-if idade >= 18:
-    print(f"{nome} é maior de idade , JÁ PODE tirar a carteira!!!")
-
-else:
-    print(f"{nome} ainda é menor de idade, NÃO pode tirar a carteira!")

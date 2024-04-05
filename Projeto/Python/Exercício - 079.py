c05f4dd6181deee4bee6b5dbe7c8afe96199f960
Rodrygo88 <2rodrigosilva2@gmail.com>
2024-04-05 00:03:08 -0300

    add

--- Projeto/Python/Exercício - 079.py
+++ Projeto/Python/Exercício - 079.py
@@ -1,1 +1,22 @@
 print("====== DESAFIO 079 ======")  
+
+lista = []
+
+while True:
+    
+    valor = int(input("Digite um número: "))
+
+    if valor in lista:
+        print("Valor já existente na lista! Não adicionado...")
+    else:
+        lista.append(valor)
+    
+    opção = str(input("Quer continuar? S/N ")).strip().upper()[0]
+    
+    while opção not in "SN":
+        opção = str(input("DE NOVO, quer continuar? S/N ")).strip().upper()[0]
+
+    if opção == "N":
+        break
+
+print(f"Valores: {sorted(lista)}")

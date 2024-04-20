dc0a6fd551457f5f468088cb543763130704047b
Rodrygo88 <2rodrigosilva2@gmail.com>
2024-04-19 23:40:39 -0300

    Update Exercício - 080.py

--- Projeto/Python/Exercício - 080.py
+++ Projeto/Python/Exercício - 080.py
@@ -1,6 +1,6 @@
 print("====== DESAFIO 080 ======")  
 
-lista = []
+"""lista = []
 
 
 for c in range(0, 5):
@@ -60,5 +60,22 @@ for c in range(0, 5):
                 print(c)
                 lista.insert(2, valor)
                 print("Adicionado na posição 2 da lista...")
-                print(lista)
-            
+                print(lista)"""
+
+lista = []
+
+for c in range(0,5):
+    n = int(input("Valor: "))
+
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                break
+            pos += 1
+    
+    print(lista)
+    

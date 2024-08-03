3f6e52b399711c8bb380aebd3c6f5fbe68ed9c8c
Rodrygo88 <2rodrigosilva2@gmail.com>
2024-08-03 11:03:38 -0300

    Create Exercício - 113.py

---
+++ Projeto/Python/Exercício - 113.py
@@ -0,0 +1,30 @@
+print("====== DESAFIO 113 ======") 
+
+def leiaInt(texto="Digite um número inteiro: "):
+      while True:
+            try:
+                  digito = int(input(texto))
+            except:
+                  print("ERRO")
+            else:
+                  break
+      return digito
+
+def leiaFloat(texto="Digite um número float: "):
+      while True:
+            try:
+                  digito = float(input(texto))
+            except:
+                  print("ERRO")
+            else:
+                  break
+      return digito
+      
+      
+
+
+n = leiaInt("Digite um número inteiro: ")
+
+n2 = leiaFloat("Digite um número float: ")
+
+print(f"O número inteiro digitado foi o {n} e o float foi {n2}")
